a67ba2e5018d1db409d2a6ff38666c2f26bc39e0
Naruse-Kazuki <kuroraido@icloud.com>
2020-05-02 23:58:07 +0900

    AddScaffod

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
 //The main function is the starting point for all our flutter apps.
-void main() => runApp(
-      MaterialApp(
-        home: Center(
-          child: Text("Hello World"),
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I Am Rich"),
         ),
       ),
-    );
+    ),
+  );
+}
